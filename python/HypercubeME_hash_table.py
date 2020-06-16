0ac7ab7b460e69a86009bb1ad6adb80c6ff43ab5
Aleksandr Poslavskii <slink21@mail.ru>
2020-06-16 16:43:04 +0300

    update py script

--- python/HypercubeME_hash_table.py
+++ python/HypercubeME_hash_table.py
@@ -4,59 +4,73 @@ import sys
 import time
 
 
-def load_dataset(path: str) -> dict:
-    with open(path) as fh:
-        omics_list = []
+def load_dataset(sequences_path: str) -> (dict, int):
+    data_list = []
+    with open(sequences_path) as fh:
         fh.readline()
         for line in fh:
             first = line.split()[0]
-            omics_list.append(first)
-    omics_list.sort()  # Just to make sure it is already sorted. It is fast.
-    return {('',): omics_list}
+            data_list.append(first)
+    data_list.sort()
+    return {('',): data_list}, len(data_list[0])
 
 
-def apply_mutation(genotype: str, mutation: tuple) -> str:
-    return genotype[:mutation[0]] + mutation[1] + genotype[mutation[0] + 1:]
-
-
-def process_genotype(key: tuple, genotype: str, genes: set, mutations: set,
-                     result: dict):
-    for mutation in mutations:
-        if genotype[mutation[0]] != mutation[1]:
-            new_diagonal_point = mutation[1] + \
-                                 str(mutation[0]) + \
-                                 genotype[mutation[0]]  # only if genes were sorted
-            if new_diagonal_point > key[-1]:
-                mutated = apply_mutation(genotype, mutation)
-                if mutated in genes:
-                    new_diagonal = key + (sys.intern(new_diagonal_point),)
-                    offset = sys.intern(genotype[:mutation[0]] + '*' +
-                                        genotype[mutation[0] + 1:])
-                    if new_diagonal in result:
-                        result[new_diagonal].append(offset)
+def process_diagonal(diag: tuple, sequences: list, seq_len: int, result: dict):
+    diag_end = int(diag[-1][1:-1]) + 1 if diag[-1] else 0
+    seq_set = set()
+    mutations = set()
+    for seq in sequences:
+        for mut in mutations:
+            if seq[mut[0]] != mut[1]:
+                if seq[:mut[0]] + mut[1] + seq[mut[0] + 1:] in seq_set:
+                    new_diag = diag + (sys.intern(mut[1] + str(mut[0]) +
+                                                  seq[mut[0]]),)
+                    if new_diag in result:
+                        result[new_diag].append(seq)
                     else:
-                        result[new_diagonal] = [offset]
-    genes.add(genotype)
-    for i in range(len(genotype)):
-        if genotype[i] != '*':
-            mutations.add((i, genotype[i]))
+                        result[new_diag] = [seq]
+        seq_set.add(seq)
+        for pos in range(diag_end, seq_len):
+            mutations.add((pos, seq[pos]))
 
 
-def process_diagonals(diagonals: dict) -> dict:
-    result = dict()
-    for key, value in diagonals.items():
-        genes = set()
-        mutations = set()
-        for genotype in value:
-            process_genotype(key, genotype, genes, mutations, result)
+def process_diagonals(diagonals: dict, seq_len: int) -> dict:
+    result = {}
+    for diag, sequences in diagonals.items():
+        process_diagonal(diag, sequences, seq_len, result)
     return result
+
+
+def process_dataset(sequences_path: str, dst_path: str):
+    dataset, seq_len = load_dataset(sequences_path)
+
+    dimension = 1
+    while True:
+        dataset = process_diagonals(dataset, seq_len)
+        number_diagonals = len(dataset)
+        print('Number of %2d dimensional diagonals: %10d'
+              % (dimension, number_diagonals))
+        number_hypercubes = 0
+
+        with open(os.path.join(dst_path, 'hypercubes_' + str(dimension) + '.txt'),
+                  'w') as f:
+            for diag, sequences in sorted(dataset.items(), key=lambda x: x[0]):
+                number_hypercubes += len(sequences)
+                diag_name = ':'.join(diag[1:])
+                f.write(''.join(diag_name + '\t' + seq + '\n' for seq in sequences))
+
+        print('Number of %2d dimensional hypercubes: %9d'
+              % (dimension, number_hypercubes))
+        dimension += 1
+        if number_diagonals == number_hypercubes:
+            break
 
 
 if __name__ == '__main__':
     start_time = time.time()
 
     p = argparse.ArgumentParser()
-    p.add_argument('-g', '--genotypes',
+    p.add_argument('-s', '--sequences',
                    help='the filename with the list of measured genotypes',
                    required=True)
     p.add_argument('-d', '--dst',
@@ -65,29 +79,9 @@ if __name__ == '__main__':
                    default='hypercubes')
     args = p.parse_args()
 
-    dataset = load_dataset(args.genotypes)
     if not os.path.exists(args.dst):
         os.makedirs(args.dst)
 
-    dimension = 1
-    while True:
-        dataset = process_diagonals(dataset)
-        number_diagonals = len(dataset)
-        print('Number of %2d dimensional diagonals: %10d'
-              % (dimension, number_diagonals))
-        number_hypercubes = 0
+    process_dataset(args.sequences, args.dst)
 
-        with open(os.path.join(args.dst, 'hypercubes_' + str(dimension) + '.txt'),
-                  'w') as f:
-            for diagonal, offsets in sorted(dataset.items(), key=lambda x: x[0]):
-                number_hypercubes += len(offsets)
-                diagonal_name = ':'.join(diagonal)[1:]
-                f.write(''.join(diagonal_name + '\t' + offset + '\n'
-                                for offset in offsets))
-
-        print('Number of %2d dimensional hypercubes: %9d'
-              % (dimension, number_hypercubes))
-        dimension += 1
-        if number_diagonals == number_hypercubes:
-            break
     print(time.time() - start_time)
